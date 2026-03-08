b739603bcd5f3da4435a694cba43eb3aaba51630
Pedroivo2000 <167208204+Pedroivo2000@users.noreply.github.com>
2026-03-08 13:29:38 -0300

    Delete js directory

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-// 🔐 Suas credenciais (publishable)
-const SUPABASE_URL = "https://kjigccxaggwtpoosihbl.supabase.co";
-const SUPABASE_KEY = "sb_publishable_Rzfa6WmpntiKSlpNDDDgcw_nVcY_Xt7";
-
-window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
